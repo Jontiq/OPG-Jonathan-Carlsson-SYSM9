9988ded43b03490e74a48fb6c8a57ce237f74e2b
Jontiq <99jonathancar@gmail.com>
2025-10-20 19:13:00 +0200

    Created some of the User class (but not done yet) with some parameters, still trying to figure out where to put methods and such. Created the UserManager class, and added it to app.xaml making sure that we're able to use the same instance of the object everywhere in the application later.

---
+++ OPG Jonathan Carlsson SYSM9/Managers/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using OPG_Jonathan_Carlsson_SYSM9.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OPG_Jonathan_Carlsson_SYSM9.Managers
+{
+    public class UserManager
+    {
+        public List<User> Users { get; set; } = new List<User>();
+
+        
+    }
+}
---
+++ OPG Jonathan Carlsson SYSM9/Models/User.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPG_Jonathan_Carlsson_SYSM9.Models
+{
+    public class User
+    {
+        //props
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+
+        //Metoden kanske borde ligga i viewmodel istället? flyttar sen när jag skapat viewmodeln
+        //public string Password
+        //{
+        //    get { return _password; }
+        //    set
+        //    {
+        //        if (string.IsNullOrWhiteSpace(value))
+        //        {
+        //            throw new ArgumentException("Password cannot be empty.");
+        //        }
+        //        if(value.Length < 8)
+        //        {
+        //            throw new ArgumentException("Password must be at least eight characters long.");
+        //        }
+
+        //        //Checks if password contains a digit
+        //        bool hasDigit = false;
+        //        foreach(char c in value)
+        //        {
+        //            if (char.IsDigit(c))
+        //            {
+        //                hasDigit = true;
+        //                break;
+        //            }
+        //        }
+        //        if (!hasDigit)
+        //        {
+        //            throw new ArgumentException("Password must contain at least one number.");
+        //        }
+
+        //        //Checks if password contains a special character (AKA if it's not letter or digit, meaning it would be a special character)
+        //        bool hasSpecial = false;
+        //        foreach(char c in value)
+        //        {
+        //            if (!char.IsLetterOrDigit(c))
+        //            {
+        //                hasSpecial = true;
+        //                break;
+        //            }
+        //        }
+        //        if (!hasSpecial)
+        //        {
+        //            throw new ArgumentException("Password must contain at least one special character");
+        //        }
+        //        _password = value;
+        //    }
+        //}
+
+
+        //constructor
+
+        public User(string username, string password, string country)
+        {
+            Username = username;
+            Password = password;
+            Country = country;
+        }
+
+        //methods
+        
+    }
+}
