fc3011ea3eceaf0deda2b89b9a15eee60503ce8e
Jontiq <99jonathancar@gmail.com>
2025-10-30 19:14:03 +0100

    Started building on the RecipesViewModel and bind some props :) The shown recipes also works as intended now!

--- OPG Jonathan Carlsson SYSM9/ViewModels/RecipeListWindowViewModel.cs
+++ OPG Jonathan Carlsson SYSM9/ViewModels/RecipeListWindowViewModel.cs
@@ -1,14 +1,60 @@
-﻿using System;
+﻿using OPG_Jonathan_Carlsson_SYSM9.Managers;
+using OPG_Jonathan_Carlsson_SYSM9.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OPG_Jonathan_Carlsson_SYSM9.ViewModels
 {
     public class RecipeListWindowViewModel : BaseViewModel
     {
+        //props
+        private readonly RecipeManager _recipeManager;
+        private readonly UserManager _userManager;
 
+        private User _loggedIn;
+        public User LoggedIn
+        {
+            get { return _loggedIn; }
+            set
+            {
+                _loggedIn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //Will store the observable recipes for the specific logged in user OR all if they have an Admin role.
+        public ObservableCollection<Recipe> Recipes { get; set; }
+
+        //Constructor
+
+        public RecipeListWindowViewModel()
+        {
+            _recipeManager = (RecipeManager)Application.Current.Resources["RecipeManager"];
+            _userManager = (UserManager)Application.Current.Resources["UserManager"];
+
+            //Stores who's logged in into LoggedIn
+            LoggedIn = _userManager.GetLoggedIn();
+            LoadRecipes();
+
+        }
+
+        //Sends the recipes to the ObservableCollection Recipes. If the user is Admin, then all recipes are loaded.
+        //Otherwise, only the recipes created by the user is shown.
+        private void LoadRecipes()
+        {
+            if (LoggedIn.IsAdmin)
+            {
+                Recipes = new ObservableCollection<Recipe>(_recipeManager.GetAllRecipes());
+            }
+            else
+            {
+                Recipes = new ObservableCollection<Recipe>(_recipeManager.GetByUser(LoggedIn.Id));
+            }
+        }
     }
 }
